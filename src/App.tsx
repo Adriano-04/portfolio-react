327f19820f4d98005e4b2989ae9df643d9de7bce
Adriano <adrianomarque135@gmail.com>
2025-01-07 22:19:32 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Test from './test'
+import GlobalStyle from './globalStyle'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Test />
+    </>
+  )
+}
+
+export default App
